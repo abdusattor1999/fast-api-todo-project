2fd9b8ebf150faa7f256d9332f6bd87a87ed2a76
Abdusattor <abdusattor.work@gmail.com>
2024-04-04 14:45:59 +0500

    Database changed to production version

--- src/app/database.py
+++ src/app/database.py
@@ -1,11 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_BASE_URL = "sqlite:///./database.db"
+# SQLALCHEMY_BASE_URL = "sqlite:///./database.db"
 # SQLALCHEMY_BASE_URL = 'postgresql://postgres:postgres_pass@localhost/fast_api_todo_project' # Postgresql uchun
 # SQLALCHEMY_BASE_URL = 'mysql+pymysql://root:mysql_pass@127.0.0.1:3306/fast_api_todo_project' # Mysql uchun
+SQLALCHEMY_BASE_URL = 'postgresql://ftfwefxr:Dq9iVBkGkJQa8xs1L3dzNicghwu7cLSe@batyr.db.elephantsql.com/ftfwefxr'
 
 
-engine = create_engine(SQLALCHEMY_BASE_URL, connect_args={"check_same_thread":False})
+engine = create_engine(SQLALCHEMY_BASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
